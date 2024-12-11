664d90e1bee5fdd2f920361ace2ccece49331361
Victor Chow <285050252@qq.com>
2024-12-11 14:40:20 +0800

    修复点击方法调用时的判断

--- src/main/kotlin/me/victor/fridaideaplugin/FridaScriptAction.kt
+++ src/main/kotlin/me/victor/fridaideaplugin/FridaScriptAction.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.ui.Messages
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.util.PsiTreeUtil
 import java.awt.Toolkit
 import java.awt.datatransfer.StringSelection
@@ -17,18 +18,28 @@ class FridaScriptAction : AnAction() {
         val file = e.getData(CommonDataKeys.PSI_FILE) ?: return
         val offset = editor.caretModel.offset
         val element = file.findElementAt(offset) ?: return
-        val method = PsiTreeUtil.getParentOfType(element, PsiMethod::class.java) ?: run {
-            Messages.showMessageDialog(project, "请在一个方法上右键点击。", "提示", Messages.getInformationIcon())
-            return
+        // 先判断是否是方法调用
+        val methodCall = PsiTreeUtil.getParentOfType(element, PsiMethodCallExpression::class.java)
+        if (methodCall != null) {
+            val targetMethod = methodCall.resolveMethod()
+            if (targetMethod != null) {
+                val methodInfo = extractMethodInfo(targetMethod) ?: return
+                val fridaScript = generateFridaScript(methodInfo)
+                copyToClipboard(fridaScript)
+            } else {
+                Messages.showMessageDialog(project, "无法解析方法调用", "错误", Messages.getErrorIcon())
+            }
+        } else {
+            // 如果光标在方法定义上，生成该方法的 frida 命令
+            val method = PsiTreeUtil.getParentOfType(element, PsiMethod::class.java)
+            if (method != null) {
+                val methodInfo = extractMethodInfo(method) ?: return
+                val fridaScript = generateFridaScript(methodInfo)
+                copyToClipboard(fridaScript)
+            } else {
+                Messages.showMessageDialog(project, "无法解析方法定义", "错误", Messages.getErrorIcon())
+            }
         }
-
-        val methodInfo = extractMethodInfo(method) ?: run {
-            Messages.showMessageDialog(project, "无法提取方法信息。", "错误", Messages.getErrorIcon())
-            return
-        }
-
-        val fridaScript = generateFridaScript(methodInfo)
-        copyToClipboard(fridaScript)
     }
 
     private fun extractMethodInfo(method: PsiMethod): MethodInfo? {
